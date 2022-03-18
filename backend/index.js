537da8a5b6af7810792f0fbe20f02582c322a610
Manji <manjicoder@gmail.com>
2022-03-18 20:09:18 +0530

    #45 Storing Data into the Database Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ connetToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json()) //  MiddleWare - If not use then console return undefined
+
+// Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`iNoteBook - Backend app listening at http:/localhost/${port}`)
+    console.log(`iNoteBook - Backend app listening at http:/localhost/${port}`)
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,29 +1,23 @@
 const express = require('express');
+const User = require('../models/User');
 
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-    obj = {
-        name: "Harry",
-        email: "harry@gmail.com",
-        password: "codewithharry123",
-        method: 'get'
-    }
-    // res.json({
-    //     name: 'harry'
-    // })
-    res.json(obj)
-});
+// Create User Using => Post '/api/user/' Does't Required auth
 
 router.post('/', (req, res) => {
-    obj = {
-        name: "Harry",
-        email: "harry@gmail.com",
-        password: "codewithharry123",
-        method: 'post'
-    }
-    res.json(obj)
+    const user = User(req.body);
+    user.save();
+    console.log(req.body);
+    res.send(req.body)
+});
+
+router.get('/', (req, res) => {
+    const user = User(req.body);
+    user.save();
+    console.log(req.body);
+    // res.send('Harr World!')
+    res.send(req.body)
 });
 
 module.exports = router
