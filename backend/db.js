ce68762c5d1bb3a4d4b1764ed66a36c4130e6c42
Manji <manjicoder@gmail.com>
2022-03-29 16:09:14 +0530

    #55 iNotebook React Project Setup

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // Environment Variable
 require('dotenv').config({ path: "./config.env" });
 
+// const mongoURL = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"; // Connection String => Mongo Compass
 const mongoURL = process.env.MONGO_URL; // Connection String => Mongo Compass
-// console.log(mongoURL);
+console.log(mongoURL);
 
 const connectToMongo = () => {
     mongoose.connect(mongoURL, () => {
