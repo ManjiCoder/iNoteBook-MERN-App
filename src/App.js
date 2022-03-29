d9c738f2cbb19e613966cce01d0d322424c39ab8
Manji <manjicoder@gmail.com>
2022-03-29 16:47:58 +0530

    #56 Creating Navbar and Routes & Concurrently is not using because config.env => undefined mongoURL aa raha h

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      This is iNotebook
-    </div>
+
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
